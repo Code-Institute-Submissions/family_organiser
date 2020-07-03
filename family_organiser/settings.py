cf2a774833f5b8de8d65cb4bb1d8c815f452f050
alex <alex96ford19@gmail.com>
2020-07-04 00:14:11 +0100

    Added media url and svgs to profile page with links to add forms.

--- family_organiser/settings.py
+++ family_organiser/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
@@ -153,3 +154,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
