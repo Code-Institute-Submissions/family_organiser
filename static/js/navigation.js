433e1629092801a11afd4d45ec4c275648baaf0f
alex <alex96ford19@gmail.com>
2020-07-03 15:48:37 +0100

    Added the navigation and gsap for animations.

---
+++ static/js/navigation.js
@@ -0,0 +1,15 @@
+var tl = gsap.timeline()
+
+$('.burger-container').on('click', function() {
+    if ($(this).hasClass('closed')) {
+        // open the navigation bar
+        tl.to('.mobile-navigation-links', 0.4, {transform: "translate(0vw, 0)"});
+        tl.to('.page-container', 0.4, {transform: "translate(-50vw, 0)"}, '-=0.4');
+        tl.from('.mobile-navigation-links li', 0.3, {transform: "translate(50vw, 0)", stagger: 0.05}, '-=0.2');
+    } else {
+        // close the navigaiton bar
+        tl.to('.mobile-navigation-links', 0.4, {transform: "translate(0vw, 0)"});
+        tl.to('.page-container', 0.4, {transform: "translate(0vw, 0)"}, '-=0.4');
+    }
+    $(this).toggleClass('closed')
+})
