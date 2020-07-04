c398e4ce03280948946c1629ceadabde097ca3f2
alex <alex96ford19@gmail.com>
2020-07-04 18:34:04 +0100

    Users can add or remove friends from their friend requests.

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('find_users/', views.find_users, name='find_users'),
     path('create_request/<pk>', views.create_friend_request, name='friends'),
+    path('update_friends/<operation>/<pk>', views.update_friends, name='update_friends'),
     path('requests/', views.requests, name='requests'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from .models import FriendRequests
+from .models import FriendRequests, Friend
 from django.db.models import Q
 
 def profile(request):
@@ -8,26 +8,6 @@ def profile(request):
     Display the users profile
     """
     return render(request, 'user/profile.html')
-
-def requests(request):
-    """
-    Shows the users sent friend requests for shopping and todo list
-    """
-    friend_requests = FriendRequests.objects.filter(to_user=request.user.pk)
-
-    context = {
-        'friend_requests': friend_requests,
-    }
-    return render(request, 'user/requests.html', context)
-
-def create_friend_request(request, pk):
-    """
-    Takes the request from the user and saves the requests to the database
-    """
-    requested_user = User.objects.get(pk=pk)
-    FriendRequests.objects.create(from_user=request.user, to_user=requested_user)
-
-    return redirect('find_users')
 
 
 def find_users(request):
@@ -48,3 +28,47 @@ def find_users(request):
     }
 
     return render(request, 'user/find_users.html', context)
+
+def create_friend_request(request, pk):
+    """
+    Takes the request from the user and saves the requests to the database
+    """
+    requested_user = User.objects.get(pk=pk)
+    FriendRequests.objects.create(from_user=request.user, to_user=requested_user)
+
+    return redirect('find_users')
+
+
+def requests(request):
+    """
+    Shows the users sent friend requests for shopping and todo list
+    """
+    friend_requests = FriendRequests.objects.filter(to_user=request.user.pk)
+
+    context = {
+        'friend_requests': friend_requests,
+    }
+    return render(request, 'user/requests.html', context)
+
+
+def update_friends(request, operation, pk):
+    """
+    Add or remove the chosen user from or too their friends list
+    """
+
+    new_friend = User.objects.get(pk=pk)
+
+    try:
+        friend_request = FriendRequests.objects.get(from_user=pk)
+        friend_request.delete()
+    except:
+        friend_request = None
+    
+    if operation == 'add':
+        Friend.make_friend(request.user, new_friend)
+        Friend.make_friend(new_friend, request.user)
+    elif operation == 'remove':
+        Friend.remove_friend(request.user, new_friend)
+        Friend.remove_friend(new_friend, request.user)
+        
+    return redirect('requests')
