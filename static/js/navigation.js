4cb0ca06419f55c9d55745243b694b1735e36b16
alex <alex96ford19@gmail.com>
2020-07-04 02:16:25 +0100

    Changed and fixed the navigation animation and added link for finding users page.

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -3,13 +3,13 @@ var tl = gsap.timeline()
 $(document).on('click','.burger-container', function() {
     if ($(this).hasClass('closed')) {
         // open the navigation bar
-        tl.to('.mobile-navigation-links', 0.4, {transform: "translate(0vw, 0)"});
-        tl.to('.page-container', 0.4, {transform: "translate(-50vw, 0)"}, '-=0.4');
+        tl.to('.burger-container', 0.4, {transform: "translate(-50vw, 0)"});
+        tl.to('.mobile-navigation-links', 0.4, {transform: "translate(-50vw, 0)"},'-=0.4');
         tl.from('.mobile-navigation-links li', 0.3, {transform: "translate(50vw, 0)", stagger: 0.05}, '-=0.2');
     } else {
         // close the navigaiton bar
-        tl.to('.mobile-navigation-links', 0.4, {transform: "translate(0vw, 0)"});
-        tl.to('.page-container', 0.4, {transform: "translate(0vw, 0)"}, '-=0.4');
+        tl.to('.burger-container', 0.4, {transform: "translate(0vw, 0)"});
+        tl.to('.mobile-navigation-links', 0.4, {transform: "translate(10vw, 0)"},'-=0.4');
     }
     $(this).toggleClass('closed')
 })
