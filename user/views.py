75583f1dd5db94622fa2453a60ce398159b691b3
alex <alex96ford19@gmail.com>
2020-07-04 16:21:02 +0100

    Added the find users page and searching for users function.

--- user/views.py
+++ user/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.db.models import Q
 
-# Create your views here.
 def profile(request):
     """
     Display the users profile
     """
     return render(request, 'user/profile.html')
 
+def find_users(request):
+    """
+    Search for other users and send a friend request
+    """
+    if request.method == 'GET':
+        try:
+            query = request.GET['q']
+            queries = Q(username=query) | Q(first_name=query) | Q(last_name=query)
+            all_users = User.objects.filter(queries)
+            print('search')
+        except:
+            all_users = User.objects.all()
+
+    context = {
+        'all_users': all_users,
+    }
+
+    return render(request, 'user/find_users.html', context)
