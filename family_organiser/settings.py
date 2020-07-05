b411c065683176c44ff313b2d15c4e81a5dc66ff
alex <alex96ford19@gmail.com>
2020-07-05 22:20:53 +0100

    Changed url in allowed hosts.

--- family_organiser/settings.py
+++ family_organiser/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://family-organiser.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['family-organiser.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
