c643a6bb69d61524daab171827a8386544c2d15e
alex <alex96ford19@gmail.com>
2020-07-06 19:00:50 +0100

    Fixed typos in settings and custom storages.

--- custom_storages.py
+++ custom_storages.py
@@ -2,9 +2,9 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticStorages(S3Boto3Storage):
+class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
 
 
-class MediaStorages(S3Boto3Storage):
+class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
--- family_organiser/settings.py
+++ family_organiser/settings.py
@@ -174,7 +174,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGES_BUCKET_NAME = 'family-organiser'
+    AWS_STORAGE_BUCKET_NAME = 'family-organiser'
     AWS_S3_REGION_NAME = 'EU'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
@@ -183,7 +183,7 @@ if 'USE_AWS' in os.environ:
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILES_STORAGE = 'custom_storages.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
