85f2d460c97aa6379d9ee2de6e5a05ba1020effc
alex <alex96ford19@gmail.com>
2020-07-06 18:52:49 +0100

    Getting the aws config set up ready for hosting.

--- family_organiser/settings.py
+++ family_organiser/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     'home',
     'user',
     'status',
+
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -169,3 +171,21 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGES_BUCKET_NAME = 'family-organiser'
+    AWS_S3_REGION_NAME = 'EU'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILES_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
