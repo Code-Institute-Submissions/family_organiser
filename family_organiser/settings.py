a20849beae67ef0795c32ad60a488967558cb287
alex <alex96ford19@gmail.com>
2020-07-07 19:25:29 +0100

    Added navigation logout background and media into context processors and changed src paths.

--- family_organiser/settings.py
+++ family_organiser/settings.py
@@ -81,7 +81,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -173,6 +173,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'family-organiser'
     AWS_S3_REGION_NAME = 'eu-west-2'
