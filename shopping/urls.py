277d7039dc5fb1cf9f7e61c29b447b241a6209c5
alex <alex96ford19@gmail.com>
2020-07-08 21:25:52 +0100

    Add and remove category form and view created, added some navigation on the shopping page for easier access. Also cart only display the categories being used.

--- shopping/urls.py
+++ shopping/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('shopping_page', views.shopping_page, name="shopping_page"),
     path('add_item', views.add_item, name="add_item"),
+    path('update_category/<operation>/<pk>', views.update_category, name="update_category"),
 ]
--- shopping/views.py
+++ shopping/views.py
@@ -6,10 +6,18 @@ def shopping_page(request):
 
     items = Item.objects.filter(user=request.user)
     categories = Category.objects.filter(user=request.user)
+    
+    # Find the categories being used
+    categories_used = []
+    for item in items:
+        if item.category not in categories_used:
+            categories_used.append(item.category)
+
 
     context = {
         'items': items,
         'categories': categories,
+        'categories_used': categories_used,
     }
 
     return render(request, 'shopping/shopping_page.html', context)
@@ -18,17 +26,49 @@ def add_item(request):
 
     if request.method == 'POST':
 
-        item = request.POST.get('item')
-        quantity = request.POST.get('quantity')
+        item_name = request.POST.get('item')
+        quantity = int(request.POST.get('quantity'))
         category = Category.objects.get(category=request.POST.get('category'))
 
-        new_item = Item(
-            user = request.user,
-            item = item,
-            quantity = quantity,
-            category = category, 
-        )
-        new_item.save()
-      
+        # Get users items from the database.
+        users_items = Item.objects.filter(user=request.user)
+        new_item = True
+
+        # If item in database add the quantity instead of creating a new object.
+        for item in users_items:
+            if item.item == item_name:
+                item_in_database = Item.objects.get(item=item_name)
+                item_in_database.quantity += quantity
+                item_in_database.save()
+                new_item = False
+        
+        # Add item if new
+        if new_item:
+            new_item = Item(
+                user = request.user,
+                item = item_name,
+                quantity = quantity,
+                category = category, 
+            )
+            new_item.save()
+
+    return redirect('shopping_page')
+
+def update_category(request, operation, pk):
+    
+    if request.method == 'POST':
+        print('nearly')
+        if operation == 'add':
+            new_category = Category(
+                user = request.user,
+                category = request.POST.get('category'),
+            )
+            new_category.save()
+        print('nearly')
+        if operation == 'remove':
+
+            category = Category.objects.get(pk=pk)
+            category.delete()
+            print('remove item')
 
     return redirect('shopping_page')
