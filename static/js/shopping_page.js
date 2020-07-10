481a06a4733b9d593315bdff489aefdda43b5c02
alex <alex96ford19@gmail.com>
2020-07-10 14:08:31 +0100

    Cleaned up title layout on the shopping page and added select all buttons for removing items on the shopping list.

--- static/js/shopping_page.js
+++ static/js/shopping_page.js
@@ -4,3 +4,21 @@ $(document).on('click', '.far', function() {
     $(this).toggleClass('fa-plus-square')
     $(this).toggleClass('fa-minus-square')
 });
+
+$(document).on('click', '.select-category-inputs', function() {
+    var inputClass = '.'.concat(this.id);
+    if ($(inputClass).prop('checked') == true) {
+        $(inputClass).prop('checked', false);
+    } else {
+        $(inputClass).prop('checked', true);
+    }
+});
+
+$(document).on('click', '.select-all-checkboxes', function() {
+    var inputClass = '.checkbox';
+    if ($(inputClass).prop('checked') == true) {
+        $(inputClass).prop('checked', false);
+    } else {
+        $(inputClass).prop('checked', true);
+    }
+})
