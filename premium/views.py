bf66300994cee50bb7cc97a43cecf5a5c5185449
alex <alex96ford19@gmail.com>
2020-07-15 12:08:42 +0100

    Added stripe elements

--- premium/views.py
+++ premium/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def premium_info(request):
 
-    return render(request, 'premium/premium_info.html')
+    context = {
+        'stripe_public_key': 'pk_test_Xny4SfQ3je684fNr2E6YMCSp00VgEdfufp',
+        'client_secret': 'test client secret',
+    }
+
+    return render(request, 'premium/premium_info.html', context)
