7105ba17592aab2cf2a0d62fa0f99441fd15bc4c
alex <alex96ford19@gmail.com>
2020-07-17 14:35:30 +0100

    Added new conversation page and logic for users to create new conversations.

--- message/urls.py
+++ message/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('start_conversation/', views.start_conversation, name="start_conversation"),
+    path('select_conversation/', views.select_conversation, name="select_conversation"),
+    path('new_conversation/', views.new_conversation, name="new_conversation"),
     path('conversation/<pk>', views.conversation, name="conversation"),
 ]
 
--- message/views.py
+++ message/views.py
@@ -2,9 +2,64 @@ from django.shortcuts import render
 from user.models import Friend
 from .models import Message
 from django.contrib.auth.models import User
+from user.models import UserProfile
+from django.db.models import Q
 
 # Create your views here.
-def start_conversation(request):
+def new_conversation(request):
+
+    if request.method == 'GET':
+
+        try:
+
+            query = request.GET['q']
+            queries = Q(username__startswith=query) | Q(first_name__startswith=query) | Q(last_name__startswith=query) | Q(username__startswith=query.capitalize()) | Q(first_name__startswith=query.capitalize()) | Q(last_name__startswith=query.capitalize())
+            users_friends = Friend.objects.get(current_user=request.user)
+            all_users = users_friends.users.filter(queries)
+
+            searched_users = []
+            for one_user in all_users:
+                user_profile = UserProfile.objects.get(user=one_user)
+
+                user_dict = {
+                    'first_name': one_user.first_name,
+                    'last_name': one_user.last_name,
+                    'username': one_user.username,
+                    'id': one_user.id,
+                    'user_profile': {
+                        'profile_image': user_profile.profile_image,
+                    }
+                }
+                searched_users.append(user_dict)
+        except:
+            searched_users = []
+        
+    try:
+        friends = Friend.objects.get(current_user=request.user)
+        all_friends_query_set = friends.users.all()
+        all_friends = []
+        for friend in all_friends_query_set:
+            all_friends.append(friend.username)
+    except:
+        all_friends = []
+
+    try:
+        friend_requests = FriendRequests.objects.filter(from_user=request.user)
+        friend_request_sent = []
+        for friend_request in friend_requests:
+            friend_request_sent.append(friend_request.to_user.username)
+    except:
+        friend_request_sent = []
+
+    context = {
+        'searched_users': searched_users,
+        'friends': all_friends,
+        'friend_request_sent': friend_request_sent,
+    }
+
+    return render(request, 'message/new_conversation.html', context)
+
+def select_conversation(request):
     """
     Continue a coversation that has already been started.
     """
@@ -23,7 +78,7 @@ def start_conversation(request):
         'friends': friends_list,
     }
 
-    return render(request, 'message/start_conversation.html', context)
+    return render(request, 'message/select_conversation.html', context)
 
 def conversation(request, pk):
     """
