6a13457b6acae7516cdb3e6322dd6193f6033717
alex <alex96ford19@gmail.com>
2020-08-09 23:53:48 +0100

    Added view tests for users app.

--- user/test_views.py
+++ user/test_views.py
@@ -9,12 +9,23 @@ class TestViews(TestCase):
         response = self.client.get(f'/user/family/{user.id}')
         self.assertEqual(response.status_code, 302)
 
-    # def test_get_find_users_page(self):
+    def test_get_find_users_page_user_not_authenticated(self):
+        response = self.client.get('/user/find_users/')
+        self.assertEqual(response.status_code, 302)
 
-    # def test_get_notifications_page(self):
+    def test_get_notifications_page_user_not_authenticated(self):
+        response = self.client.get('/user/notifications/')
+        self.assertEqual(response.status_code, 302)
 
-    # def test_get_profile_page(self):
+    def test_get_profile_page_user_not_authenticated(self):
+        response = self.client.get('/user/profile/')
+        self.assertEqual(response.status_code, 302)
 
-    # def test_get_settings_page(self):
+    def test_get_settings_page_user_not_authenticated(self):
+        response = self.client.get('/user/settings/')
+        self.assertEqual(response.status_code, 302)
 
-    # def test_get_view_user_profile_page(self):
+    def test_get_view_user_profile_page_user_not_authenticated(self):
+        user = create_test_user()
+        response = self.client.get(f'/user/view_user_profile/{user.id}')
+        self.assertEqual(response.status_code, 302)
