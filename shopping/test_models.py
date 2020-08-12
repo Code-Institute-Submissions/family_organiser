4298338ea481e3070c25f4694b8b914f2be66c17
alex <alex96ford19@gmail.com>
2020-08-13 00:16:47 +0100

    Added travis file and model test for shopping app.

--- shopping/test_models.py
+++ shopping/test_models.py
@@ -10,7 +10,7 @@ class TestModels(TestCase):
             user = user,
             category = 'test',
         )
-        self.assertEqual(str(category), 'test')
+        self.assertEqual(str(category), category.category)
 
     def test_item_method_returns_item(self):
         user = create_test_user('1')
@@ -23,7 +23,7 @@ class TestModels(TestCase):
             item = 'testItem',
             category = category,
         )
-        self.assertEqual(str(item), 'testItem')
+        self.assertEqual(str(item), item.item)
 
     def test_purchased_items_method_returns_item(self):
         user = create_test_user('1')
@@ -36,7 +36,7 @@ class TestModels(TestCase):
             item = 'testItem',
             category = category,
         )
-        self.assertEqual(str(purchased_item), 'testItem')
+        self.assertEqual(str(purchased_item), purchased_item.item)
         
     def test_favorite_method_returns_item(self):
         user = create_test_user('1')
@@ -49,7 +49,24 @@ class TestModels(TestCase):
             item = 'testItem',
             category = category,
         )
-        self.assertEqual(str(favourite), 'testItem')
+        self.assertEqual(str(favourite), favourite.item)
+
+    def test_partner_request_method_returns_username(self):
+        user_one = create_test_user('1')
+        user_two = create_test_user('2')
+        partner_request = PartnerRequest(
+            from_user = user_one,
+            to_user = user_two,
+        )
+        self.assertEqual(str(partner_request), user_one.username)
+
+    def test_partner_method_returns_username(self):
+        user = create_test_user('1')
+        partner = Partner(
+            current_user = user,
+        )
+        self.assertEqual(str(partner), user.username)
+
 
 
 
