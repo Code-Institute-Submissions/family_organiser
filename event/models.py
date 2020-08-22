22d4743f64103d1c24355938efd79b2aaddb7092
alex <alex96ford19@gmail.com>
2020-08-23 00:29:46 +0100

    Created model for event app and added the search user functionality on create event page.

--- event/models.py
+++ event/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Event(models.Model):
+    event_creator = models.ForeignKey(User, related_name="event_creator", on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    information = models.CharField(max_length=350)
+    header_image = models.ImageField(null=True, blank=True)
+    participants = models.ManyToManyField(User, related_name="event_participants")
+
+    @classmethod
+    def participant_accepted(cls, participant):
+        cls.participants.add(participant)
+
+    def __str__(self):
+        return self.title
+    
+class EventInvite(models.Model):
+    event = models.ForeignKey(Event, related_name="event_invite_event", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="event_invite_user", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
--- event/views.py
+++ event/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from message.functions.functions import get_searched_users
 
 # Create your views here.
 def create_event(request):
+    """
+    Create a new event and invite friends to the created event.
+    """
+    if request.method == 'GET':
+        searched_users = get_searched_users(request)
 
-    return render(request, 'event/create_event.html')
+    context = {
+        'searched_users': searched_users,
+    }
+
+    return render(request, 'event/create_event.html', context)
