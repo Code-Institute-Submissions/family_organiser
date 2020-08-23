0dda419236d9564ce41345b9c2bea7f03fa48628
alex <alex96ford19@gmail.com>
2020-08-23 01:28:47 +0100

    Broke up creat event into two pages for reusablity later on

--- event/urls.py
+++ event/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('create_event/', views.create_event, name='create_event'),
+    path('invite/<pk>', views.invite, name='invite'),
 ]
--- event/views.py
+++ event/views.py
@@ -1,17 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from message.functions.functions import get_searched_users
+from django.contrib.auth.decorators import login_required
 from .forms import EventForm
+from .models import Event
 
-# Create your views here.
+
+@login_required
 def create_event(request):
     """
     Create a new event and invite friends to the created event.
     """
-    # Find the friends searched by the user.
-    if request.method == 'GET':
-        searched_users = get_searched_users(request)
-    else:
-        searched_users = []
 
     # Create the event.
     if request.method == 'POST':
@@ -22,9 +20,33 @@ def create_event(request):
             event_form.event_creator = request.user
             event_form.save()
 
+            return redirect('invite', event_form.pk)
+
     context = {
-        'searched_users': searched_users,
         'create_event_form': EventForm,
     }
 
     return render(request, 'event/create_event.html', context)
+
+
+@login_required
+def invite(request, pk):
+    """
+    Invite friends to an event passed into the view.
+    """
+    # Find the friends searched by the user.
+    if request.method == 'GET':
+        searched_users = get_searched_users(request)
+    else:
+        searched_users = []
+
+    # Find the chosen event
+    event = get_object_or_404(Event, pk=pk)
+
+    context = {
+        'event': event,
+        'searched_users': searched_users,
+    }
+
+    return render(request, 'event/invite.html', context)
+
