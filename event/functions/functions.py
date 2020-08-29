b549d788042f7727983042ad0f670871ec76d6c8
alex <alex96ford19@gmail.com>
2020-08-29 17:58:11 +0100

    Added if statement to count down in the event section for events that have passed and on event day.

--- event/functions/functions.py
+++ event/functions/functions.py
@@ -6,10 +6,18 @@ def add_count_down_to_events(events):
     events_with_count_down = []
     
     for event in events:
+        # find the days betweent now and the event.
         event_date = date(event.event_date.year, event.event_date.month, event.event_date.day)
         time_between = event_date - todays_date
         days_between = time_between.days
 
+        # format count down
+        count_down = '{} Days'.format(str(days_between))
+        if days_between == 0:
+            count_down = '{} is today!'.format(event.title)
+        elif days_between < 0:
+            count_down = '{} was {} days ago.'.format(event.title, days_between)
+        
         event_dict = {
             'id': event.id,
             'event_creator': event.event_creator,
@@ -18,7 +26,7 @@ def add_count_down_to_events(events):
             'header_image': event.header_image,
             'participants': event.participants,
             'event_date': event.event_date,
-            'count_down': '{} Days'.format(str(days_between)),
+            'count_down': count_down,
         }
 
         events_with_count_down.append(event_dict)
