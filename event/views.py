01366d2d7a1917471bd31240588fc9e72a7209db
alex <alex96ford19@gmail.com>
2020-09-20 17:50:27 +0100

    Added if statement to event views to make sure only premium users can access. Also make a start on testing.md page.

--- event/views.py
+++ event/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from .forms import EventForm
 from .models import Event, EventInvite
 from .functions.functions import *
-
+from user.functions.functions import get_users_profile
 import datetime
 import os
 from os import path
@@ -25,43 +25,54 @@ def menu(request):
     """
     View all the users events, created or invited.
     """
-    # Get all events the users has been invited to.
-    all_events = Event.objects.all()
-    events = []
-    for event in all_events:
-        if request.user in event.participants.all():
-            events.append(event)
+    user_profile = get_users_profile(request.user.id)
 
-    events = add_count_down_to_events(events)
+    if user_profile.premium:
+        # Get all events the users has been invited to.
+        all_events = Event.objects.all()
+        events = []
+        for event in all_events:
+            if request.user in event.participants.all():
+                events.append(event)
 
-    context = {
-        'events': events,
-    }
+        events = add_count_down_to_events(events)
 
-    return render(request, 'event/menu.html', context)
+        context = {
+            'events': events,
+        }
+
+        return render(request, 'event/menu.html', context)
+    else:
+        return redirect('premium_info')
 
 @login_required
 def create_event(request):
     """
     Create a new event and invite friends to the created event.
     """
-    # Create the event.
-    if request.method == 'POST':
-        form = EventForm(request.POST, request.FILES)
-        if form.is_valid():
-            print(request.FILES)
-            event_form = form.save(commit=False)
-            event_form.event_creator = request.user
-            event_form.save()
-            event_form.participants.add(request.user)
+    user_profile = get_users_profile(request.user.id)
 
-            return redirect('invite', event_form.pk, 0)
+    if user_profile.premium:
+        # Create the event.
+        if request.method == 'POST':
+            form = EventForm(request.POST, request.FILES)
+            if form.is_valid():
+                print(request.FILES)
+                event_form = form.save(commit=False)
+                event_form.event_creator = request.user
+                event_form.save()
+                event_form.participants.add(request.user)
 
-    context = {
-        'create_event_form': EventForm,
-    }
+                return redirect('invite', event_form.pk, 0)
 
-    return render(request, 'event/create_event.html', context)
+        context = {
+            'create_event_form': EventForm,
+        }
+
+        return render(request, 'event/create_event.html', context)
+    else:
+        return redirect('premium_info')
+
 
 
 @login_required
@@ -69,79 +80,99 @@ def invite(request, event_pk, user_pk):
     """
     Invite friends to an event passed into the view.
     """
-    # Find the friends searched by the user.
-    if request.method == 'GET':
-        searched_users = get_searched_users(request)
+    user_profile = get_users_profile(request.user.id)
+
+    if user_profile.premium:
+        # Find the friends searched by the user.
+        if request.method == 'GET':
+            searched_users = get_searched_users(request)
+        else:
+            searched_users = []
+
+        event = get_object_or_404(Event, pk=event_pk)
+        invited_users = EventInvite.objects.filter(event=event)
+
+        # Send invite to user.
+        if request.method == 'POST':
+            EventInvite.create_invitation(get_object_or_404(User, pk=user_pk), event)
+
+        context = {
+            'event': event,
+            'searched_users': searched_users,
+            'invited_users': invited_users,
+        }
+
+        return render(request, 'event/invite.html', context)
     else:
-        searched_users = []
-
-    event = get_object_or_404(Event, pk=event_pk)
-    invited_users = EventInvite.objects.filter(event=event)
-
-    # Send invite to user.
-    if request.method == 'POST':
-        EventInvite.create_invitation(get_object_or_404(User, pk=user_pk), event)
-
-    context = {
-        'event': event,
-        'searched_users': searched_users,
-        'invited_users': invited_users,
-    }
-
-    return render(request, 'event/invite.html', context)
-
+        return redirect('premium_info')
 
 @login_required
 def event(request, pk):
     """
     View the selected event and create messages on the page for only attendees to see.
     """
+    user_profile = get_users_profile(request.user.id)
 
-    event = Event.objects.get(pk=pk)
-    event = add_count_down_to_events([event])
+    if user_profile.premium:
+        event = Event.objects.get(pk=pk)
+        event = add_count_down_to_events([event])
 
-    context = {
-        'event': event[0],
-    }
+        context = {
+            'event': event[0],
+        }
 
-    return render(request, 'event/event.html', context)
-
+        return render(request, 'event/event.html', context)
+    else:
+        return redirect('premium_info')
+        
 
 @login_required
 def remove_event(request, pk):
     """
     Remove an event from the database.
     """
+    user_profile = get_users_profile(request.user.id)
 
-    event = get_object_or_404(Event, pk=pk)
-    event.delete()
+    if user_profile.premium:
+        event = get_object_or_404(Event, pk=pk)
+        event.delete()
 
-    return redirect('menu')
+        return redirect('menu')
+    else:
+        return redirect('premium_info')
 
 @login_required
 def view_invite(request, pk):
     """
     View the invite page where the user can accept or decline the request.
     """
+    user_profile = get_users_profile(request.user.id)
 
-    context = {
-        'event': get_object_or_404(Event, pk=pk)
-    }
+    if user_profile.premium:
+        context = {
+            'event': get_object_or_404(Event, pk=pk)
+        }
 
-    return render(request, 'event/view_invite.html', context)
+        return render(request, 'event/view_invite.html', context)
+    else:
+        return redirect('premium_info')
 
 @login_required
 def edit_invite(request, pk, operation):
     """
     Accepting or declining the event invitation.
     """
+    user_profile = get_users_profile(request.user.id)   
 
-    if operation == 'accept':
-        Event.participant_accepted(pk, request.user)
-        return redirect('event', pk)
+    if user_profile.premium:
+        if operation == 'accept':
+            Event.participant_accepted(pk, request.user)
+            return redirect('event', pk)
 
-    if operation == 'decline':
-        return redirect('profile')
+        if operation == 'decline':
+            return redirect('profile')
+    else:
+        return redirect('premium_info')
 
 
 # All the views to add the event to the google calendar api.
@@ -150,89 +181,94 @@ def authorise(request, event):
     """
     Authorise the appilcate with Oauth
     """
-    print(event)
+    user_profile = get_users_profile(request.user.id)   
 
-    event = json.loads(event)
+    if user_profile.premium:
+        event = json.loads(event)
+        request.session['event'] = event
 
-    request.session['event'] = event
+        SCOPES = ['https://www.googleapis.com/auth/calendar']
+        
+        config = json.loads(os.environ['CRED'])
 
-    SCOPES = ['https://www.googleapis.com/auth/calendar']
-    
-    config = json.loads(os.environ['CRED'])
+        flow = google_auth_oauthlib.flow.Flow.from_client_config(
+        config, SCOPES)
 
-    flow = google_auth_oauthlib.flow.Flow.from_client_config(
-    config, SCOPES)
+        flow.redirect_uri = 'https://family-organiser.herokuapp.com/event/oauth_2_call_back/'
 
-    flow.redirect_uri = 'https://family-organiser.herokuapp.com/event/oauth_2_call_back/'
+        authorization_url, state = flow.authorization_url(
+            access_type='offline',
+            include_granted_scopes='true')
 
-    authorization_url, state = flow.authorization_url(
-        access_type='offline',
-        include_granted_scopes='true')
+        request.session['state'] = state
 
-    request.session['state'] = state
-
-    return redirect(authorization_url)
+        return redirect(authorization_url)
+    else:
+        return redirect('premium_info')
 
 
 def oauth_2_call_back(request):
 
-    SCOPES = ['https://www.googleapis.com/auth/calendar']
+    user_profile = get_users_profile(request.user.id)   
 
-    state = request.session['state']
+    if user_profile.premium:
+        SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-    config = json.loads(os.environ['CRED'])
+        state = request.session['state']
 
-    flow = google_auth_oauthlib.flow.Flow.from_client_config(
-    config, SCOPES, state=state)
+        config = json.loads(os.environ['CRED'])
 
-    flow.redirect_uri = 'https://family-organiser.herokuapp.com/event/oauth_2_call_back/'
+        flow = google_auth_oauthlib.flow.Flow.from_client_config(
+        config, SCOPES, state=state)
 
-    authorization_response = request.get_full_path()
+        flow.redirect_uri = 'https://family-organiser.herokuapp.com/event/oauth_2_call_back/'
 
-    flow.fetch_token(authorization_response=authorization_response)
+        authorization_response = request.get_full_path()
 
-    credentials = flow.credentials
+        flow.fetch_token(authorization_response=authorization_response)
 
-    service = build('calendar', 'v3', credentials=credentials)
+        credentials = flow.credentials
 
-    event_date = request.session['event']['date']
-    event_start_time = request.session['event']['start_time']
-    event_end_time = request.session['event']['end_time']
+        service = build('calendar', 'v3', credentials=credentials)
 
-    event = {
-        'summary': request.session['event']['title'],
-        'location': request.session['event']['location'],
-        'description': request.session['event']['description'],
-        'start': {
-            'dateTime': '{}T00:00:00-{}'.format(event_date, event_start_time),
-            'timeZone': "Europe/London",
-        },
-        'end': {
-            'dateTime': '{}T00:00:00-{}'.format(event_date, event_end_time),
-            'timeZone': "Europe/London",
-        },
-        'attendees': [
-            {'email': 'lpage@example.com'},
-            {'email': 'sbrin@example.com'},
-        ],
-        'reminders': {
-            'useDefault': False,
-            'overrides': [
-            {'method': 'email', 'minutes': 24 * 60},
-            {'method': 'popup', 'minutes': 10},
+        event_date = request.session['event']['date']
+        event_start_time = request.session['event']['start_time']
+        event_end_time = request.session['event']['end_time']
+
+        event = {
+            'summary': request.session['event']['title'],
+            'location': request.session['event']['location'],
+            'description': request.session['event']['description'],
+            'start': {
+                'dateTime': '{}T00:00:00-{}'.format(event_date, event_start_time),
+                'timeZone': "Europe/London",
+            },
+            'end': {
+                'dateTime': '{}T00:00:00-{}'.format(event_date, event_end_time),
+                'timeZone': "Europe/London",
+            },
+            'attendees': [
+                {'email': 'lpage@example.com'},
+                {'email': 'sbrin@example.com'},
             ],
-        },
-        }
+            'reminders': {
+                'useDefault': False,
+                'overrides': [
+                {'method': 'email', 'minutes': 24 * 60},
+                {'method': 'popup', 'minutes': 10},
+                ],
+            },
+            }
 
-    event = service.events().insert(calendarId='primary', body=event).execute()
+        event = service.events().insert(calendarId='primary', body=event).execute()
 
-    calendar_link = event.get('htmlLink') 
-    
+        calendar_link = event.get('htmlLink')         
 
-    return redirect('calendar_confirmed')
+        return redirect('calendar_confirmed')
+    else:
+        return redirect('premium_info')
 
 
 def calendar_confirmed(request):
-
 
     return render(request, 'event/calendar_confirmed.html')
--- readme/testing.md
+++ readme/testing.md
@@ -1,1 +1,81 @@
 # Manual Testing
+
+## Creating an account
+
+#### Form
+
+1. From the home page press 'Login'.
+2. Press 'Sign Up'.
+3. Try and submit empty form.
+4. Fill out form trying to submit after each field is complete.
+5. After all fields are completed press 'Sign up' and make sure your redirected to the profile page.
+
+#### Google login
+
+1. From the home page press 'Login'.
+2. Press 'Sign in with Google'.
+3. If your not already logged into your google account, login then your account will be created.
+
+#### Removing an account
+
+1. Navigatate to the settings page.
+2. Press 'Delete Account' and then 'Delete Account'.
+3. Try and sign in with old login details.
+
+## Premium user
+
+#### Try accessing premium features as a non premium user
+
+1. Go to 'shopping'.
+2. Press 'partners'.
+3. Press 'Inights'.
+4. Go to 'Events'.
+5. Make sure that all these pages returned the premium feature page.
+
+#### Paying for premium
+
+1. If on mobile start by press the navigation button.
+2. On the 'Why Premium?' section press 'more' (This page will also be displayed whenever a premium feature is slected)
+3. Press 'Upgrade £1.99'.
+4. Try and make payment with an empty field.
+5. Try and make the payment with invalid card details.
+6. Add the following card details '4242424242424242 04/24 242 42424'.
+7. Then press 'Make Payment'.
+8. The 'Payment Successful' Page will be displayed.
+
+## Shopping 
+
+#### Creating a category
+
+1. Navigate to the shopping page after you've logged in.
+2. Try and submit an empty form.
+3. Fill out the required field and press 'Add Category'.
+
+#### Removing a category
+
+1. Press 'Edit Categories' and press the bin icon.
+2. Press 'Remove Category'.
+3. Check all items with this category and the category will be removed.
+
+#### Adding a new item
+
+1. Press the 'Add New Item' '+' button.
+2. Try and submit the form with empty fields.
+3. After all fields are completed press 'Add To List'.
+4. Make sure item has been added to the shopping list.
+5. Make sure a button has been added above the shopping list for this item.
+
+#### Quick adding the item
+
+1. Press the button above the shopping list of the new items created.
+2. Make sure the item has been added to the shopping list without the page reloading.
+
+#### Incrementing and Decrementing items
+
+1. Press the down arrow on an item untill the item reaches '0' and gets removed from the list.
+2. Press the up arrow on an item and make sure the item quantity increases.
+
+#### Adding a shopping partner
+
+#### 
+
