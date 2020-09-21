9d67f28045936cd8e0f0d7c3932e78b3d6be9817
alex <alex96ford19@gmail.com>
2020-09-21 20:01:11 +0100

    Fixed event test not passing because the views have to changed to only allow premium users.

--- event/tests_views.py
+++ event/tests_views.py
@@ -5,13 +5,9 @@ from .models import Event
 class TestViews(TestCase):
 
     # menu page
-    def test_get_menu_page_user_not_authenticated(self):
-        response = self.client.get('/event/menu/')
-        self.assertEqual(response.status_code, 302)
-
-    def test_get_menu_page_user_authenticated(self):
+    def test_get_menu_page_user_authenticated_not_premium(self):
         self.client = Client()
         self.user = User.objects.create_user('test', 'test@test.com', 'Password777')
         self.client.login(username='test', password='Password777')
         response = self.client.get('/event/menu/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
